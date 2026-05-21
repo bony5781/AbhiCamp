1e7f320bc9bea441bc97901cbb3a5b2c91d15355
Abhinav Bhowmik <89747136+bony5781@users.noreply.github.com>
2026-05-21 09:25:58 +0530

    Update showPageMap.js

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,19 +1,15 @@
-mapboxgl.accessToken = mapToken;
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v12', // style URL
-center: campground.geometry.coordinates, // starting position [lng, lat]
-zoom: 9, // starting zoom
-});
+const coordinates = campground.geometry.coordinates;
 
-map.addControl(new mapboxgl.NavigationControl());
+const map = L.map('map').setView(
+    [coordinates[1], coordinates[0]],
+    10
+);
 
-new mapboxgl.Marker()
-    .setLngLat(campground.geometry.coordinates)
-    .setPopup(
-        new mapboxgl.Popup({offset: 25})
-        .setHTML(
-            `<h3>${campground.title}</h3><p>${campground.location}</p>`
-        )
-    )
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+L.marker([coordinates[1], coordinates[0]])
     .addTo(map)
+    .bindPopup(`<b>${campground.title}</b><br>${campground.location}`)
+    .openPopup();
